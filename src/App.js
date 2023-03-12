3b48dd67ecaa758058c630cc0fd1c73003c829e5
Murli Patel <patelmurli15@gmail.com>
2023-03-12 21:59:13 +0530

    manage student code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Dashboard from './components/Dashboard';
+import ManageStudent from './components/ManageStudent';
 
 function App() {
   return (
     <div className="App">
       <Dashboard />
+      <ManageStudent />
     </div>
   );
 }
